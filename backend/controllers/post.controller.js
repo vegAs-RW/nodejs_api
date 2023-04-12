5d62cc24778b626c68d0e061f63755d473a6fd68
Jordan <jordan.pouchot2@gmail.com>
2023-04-12 15:18:40 +0200

    add controller for like and unlike post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,12 +1,12 @@
 const PostModel = require('../models/post.model')
 
-
+// Controller for get all posts
 module.exports.getPosts = async (req, res) => {
     const posts = await PostModel.find();
     res.status(200).json(posts)
 }
 
-
+// Controller for post a new post
 module.exports.setPosts = async (req, res) => {
     if(!req.body.message) {
         res.status(400).json({message:"Merci d'ajouter un message"})
@@ -18,6 +18,7 @@ module.exports.setPosts = async (req, res) => {
     res.status(200).json(post);
 }
 
+// Controller for edit a post
 module.exports.editPosts = async (req, res) => {
     const post = await PostModel.findById(req.params.id)
 
@@ -33,6 +34,7 @@ module.exports.editPosts = async (req, res) => {
     res.status(200).json(updatePost)
 }
 
+// Controller for delete a post
 module.exports.deletePosts = async (req, res) => {
     const post = await PostModel.findById(req.params.id)
 
@@ -41,4 +43,30 @@ module.exports.deletePosts = async (req, res) => {
     }
     await post.deleteOne();
     res.status(200).json("Message supprimé" + req.params.id)
+}
+
+// Controller for add a liker in a array "likers" by his userId
+module.exports.likePosts = async (req, res) => {
+    try {
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {$addToSet: {likers: req.body.userId}},
+            {new: true}
+        ).then(data => res.status(200).json(data))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+// Controller for remove a liker in a array "likers" by his userId
+module.exports.dislikePosts = async (req, res) => {
+    try {
+        await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {$pull: {likers: req.body.userId}},
+            {new: true}
+        ).then(data => res.status(200).json(data))
+    } catch (err) {
+        res.status(400).json(err)
+    }
 }
