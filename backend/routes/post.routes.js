a3ce377df9b7657dd7b8ee322f64b3caf3e63db4
Jordan <jordan.pouchot2@gmail.com>
2023-04-12 15:19:17 +0200

    finish routes whith controller

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const { setPosts, getPosts, editPosts, deletePosts } = require('../controllers/post.controller');
+const { setPosts, getPosts, editPosts, deletePosts, likePosts, dislikePosts } = require('../controllers/post.controller');
 
 const router = express.Router();
 
 router.get("/", getPosts);
-
 router.post("/", setPosts);
-
 router.put("/:id", editPosts);
-
 router.delete("/:id", deletePosts)
-
-router.patch("/like-post/:id", (req, res) => {
-    res.json({message: "post liké : id : " + req.params.id});
-});
-
-router.patch("/dislike-post/:id", (req, res) => {
-    res.json({message: "post disliké : id : " + req.params.id});
-});
+router.patch("/like-post/:id", likePosts);
+router.patch("/dislike-post/:id", dislikePosts);
 
 
 module.exports = router
