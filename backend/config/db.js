afa129e326fffa84f97154bda55dcb3118895102
Jordan <jordan.pouchot2@gmail.com>
2023-04-12 14:50:02 +0200

    modif config db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
     try {
         mongoose.set('strictQuery', false);
-        mongoose.connect(process.env.MONGO_URI, () => {
-            console.log('connect to database');
-        });
+        mongoose.connect(process.env.MONGO_URI);
+        console.log('mongo connecté');
     } catch (err) {
         console.log(err);
         process.exit();
